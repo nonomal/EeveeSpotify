4e05239a94c34d4ac8db0e421cb7f441f55bcb59
eevee <eeveerlm@gmail.com>
2024-11-18 23:15:51 +0300

    bump

--- Sources/EeveeSpotify/Tweak.x.swift
+++ Sources/EeveeSpotify/Tweak.x.swift
@@ -11,7 +11,7 @@ func exitApplication() {
 struct PremiumPatchingGroup: HookGroup { }
 
 struct EeveeSpotify: Tweak {
-    static let version = "5.8"
+    static let version = "5.8.1"
     static let isOldSpotifyVersion = NSClassFromString("Lyrics_NPVCommunicatorImpl.LyricsOnlyViewController") == nil
     
     init() {
