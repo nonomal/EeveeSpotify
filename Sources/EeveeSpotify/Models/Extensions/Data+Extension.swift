b06b3ec5b0e21f993837c9d3a2d44ac764333bb3
eevee <eeveerlm@gmail.com>
2025-01-01 23:52:40 +0300

    cydia icons

--- Sources/EeveeSpotify/Models/Extensions/Data+Extension.swift
+++ Sources/EeveeSpotify/Models/Extensions/Data+Extension.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 extension Data {
-
     static func random(_ length: Int) -> Data {
         return Data((0 ..< length).map { _ in UInt8.random(in: UInt8.min ... UInt8.max) })
     }
